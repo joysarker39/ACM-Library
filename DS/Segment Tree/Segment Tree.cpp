5b360506ada6c476f8abc7a75835fdbf1564df79
Joy Sarker <joysarker39@gmail.com>
2021-09-20 02:03:54 +0600

    Update Segment Tree.cpp

--- DS/Segment Tree/Segment Tree.cpp
+++ DS/Segment Tree/Segment Tree.cpp
@@ -1,53 +1,27 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define fast_cin ios_base::sync_with_stdio(false);cin.tie(NULL)
-#define D(x) cerr<<__LINE__<<" : "<<#x<<" -> "<<x<<endl
-
-const int MAX = 1e5+7;            ///Check Every Time
-int t=1, n, m, l, k, tc, q;
-int arr[MAX];
-char s[MAX];
-
 struct Node {
-    int mp[26], d;
-    Node() {
-        for(int i = 0; i < 26; i++)
-            mp[i] = 0;
-        d = 0;
+    int sum = 0, mx, mn;
+    Node(int a = 0, int b = 0, int c = 0) {
+        sum = a;
+        mx = b;
+        mn = c;
     }
 } tree[MAX*4];
 
 Node _merge(Node A, Node B) {
-    if(A.d == 26) return A;
-    if(B.d == 26) return B;
     Node C;
-    for(int i = 0; i < 26; i++){
-        C.mp[i] = (A.mp[i] or B.mp[i]);
-        C.d += C.mp[i];
-    }
+    C.sum = A.sum + B.sum;
+    C.mx =  max(A.mx, B.mx);
+    C.mn = min(A.mn, B.mn);
     return C;
 }
 
 void init(int idx, int st, int ed) {
-    if(st == ed) {
-        tree[idx].mp[arr[st]] = 1;
-        tree[idx].d = 1;
-        return;
-    }
+    if(st == ed) {tree[idx] = {arr[st], arr[st], arr[st]};return;}
     int mid = (st + ed) >> 1, L = idx << 1, R = L + 1;
     init(L, st, mid);
     init(R, mid+1, ed);
     tree[idx] = _merge(tree[L], tree[R]);
 }
-/// for single value
-//int range_query1(int idx, int st, int ed, int i, int j) {
-//    if(i > ed or j < st) return 0;
-//    if( i <= st and ed <= j) return tree[idx].distinct;
-//    int mid = (st + ed) >> 1, L = idx << 1, R = L + 1;
-//    int lside = range_query1(L, st, mid, i, j);
-//    int rside = range_query1(R, mid+1, ed, i, j);
-//    return lside + rside;
-//}
 
 Node range_query(int idx, int st, int ed, int i, int j) {
     if(st == i and ed == j) return tree[idx];
@@ -57,15 +31,20 @@ Node range_query(int idx, int st, int ed, int i, int j) {
     return _merge(range_query(L, st, mid, i, mid), range_query(R, mid+1, ed, mid+1, j));
 }
 
-void update(int idx, int st, int ed, int pos, char val) {
-    if(st == ed and ed == pos) {
-        tree[idx].mp[arr[pos]] = 0;
-        arr[pos] = val;
-        tree[idx].mp[val] = 1;
-        return;
-    }
+void point_update(int idx, int st, int ed, int pos, int val) {
+    if(st == ed and ed == pos) {tree[idx] = {arr[st], arr[st], arr[st]};return;}
     int mid = (st + ed) >> 1, L = idx << 1, R = L + 1;
-    if(mid < pos) update(R, mid+1, ed, pos, val);
-    else update(L, st, mid, pos, val);
+    if(mid < pos) point_update(R, mid+1, ed, pos, val);
+    else point_update(L, st, mid, pos, val);
     tree[idx] = _merge(tree[L], tree[R]);
 }
+
+/// for single value
+//int range_query1(int idx, int st, int ed, int i, int j) {
+//    if(i > ed or j < st) return 0;
+//    if( i <= st and ed <= j) return tree[idx].distinct;
+//    int mid = (st + ed) >> 1, L = idx << 1, R = L + 1;
+//    int lside = range_query1(L, st, mid, i, j);
+//    int rside = range_query1(R, mid+1, ed, i, j);
+//    return lside + rside;
+//}
